480fb3aec0d267ea1a757bc7c21dbc210744c7eb
AngelHirales <115297427+AngelHirales@users.noreply.github.com>
2024-06-28 12:08:44 -0700

    Update#1 separador.py
    Se agrego la funcionalidad de validar caracteres

--- separador.py
+++ separador.py
@@ -1,12 +1,19 @@
 # Separador de caracteres
-def separador_de_caracteres(cadena):
+import re
 
-    caracteres = []
-    for caracter in cadena:
-        caracteres.append(caracter)
-    return caracteres
+def separador_de_caracteres(cadena):
+    return[caracter for caracter in cadena]
+
+def validar_caracteres(cadena):
+    caracteres_validos = re.compile("^[ a-zñA-ZÑáéíóúüÁÉÍÓÚ0-9.,;:!*-¿?\'\"()\t]+$")
+    if caracteres_validos.match(cadena): return True
+    return False
 
 cadena_escrita = input("Escriba una cadena de texto: ")
 
-separar_caracteres = separador_de_caracteres(cadena_escrita)
-print("La cadena escrita es la siguiente: ", separar_caracteres)
+if validar_caracteres(cadena_escrita):
+    caracteres_separados = separador_de_caracteres(cadena_escrita)
+    print("✅ La cadena escrita es la siguiente: ", caracteres_separados)
+else:
+    print("❌ Error. La cadena ingresada contiene caracteres no validos.")
+
