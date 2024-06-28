c39e4fbe3a553ae319f8d57b869ddccc84958914
AngelHirales <115297427+AngelHirales@users.noreply.github.com>
2024-06-28 13:07:39 -0700

    Update#1 separador.py
    Se agrego la funcion de validar los caracteres de la cadena de texto

--- separador.py
+++ separador.py
@@ -1,12 +1,13 @@
-# Separador de caracteres
+# Separar y validar los caracteres de una cadena de texto
 import re
 
 def separador_de_caracteres(cadena):
     return[caracter for caracter in cadena]
 
 def validar_caracteres(cadena):
-    caracteres_validos = re.compile("^[ a-zñA-ZÑáéíóúüÁÉÍÓÚ0-9.,;:!*-¿?\'\"()\t]+$")
-    if caracteres_validos.match(cadena): return True
+    caracteres_validos = re.compile(r"^[ a-zñA-ZÑáéíóúüÁÉÍÓÚ0-9.,;:!*-¿?\'\"()\t]+$")
+    if caracteres_validos.match(cadena):
+        return True
     return False
 
 cadena_escrita = input("Escriba una cadena de texto: ")
@@ -16,4 +17,3 @@ if validar_caracteres(cadena_escrita):
     print("✅ La cadena escrita es la siguiente: ", caracteres_separados)
 else:
     print("❌ Error. La cadena ingresada contiene caracteres no validos.")
-
